1433e46bace75f92b3d16d0b3e1b351353ddf4a3
mrsloppytom <140354003+mrsloppytom@users.noreply.github.com>
2023-07-31 12:16:17 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,6 @@
 //your parameter variables go here!
-
-
+let plane1X = 180; // x-coordinate of plane 1
+let plane1Y = 15; // y-coordinate of plane 1
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -14,39 +14,121 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(155, 232, 235); //(240, 255, 240)light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-  line(0, 180, 200, 0) //diagonal line left to right
-  line(0, 0, 200, 180) //diagonal line right to left
+  line(0, 180, 200, 0); //diagonal line left to right
+  line(0, 0, 200, 180); //diagonal line right to left
+
+  
+
+//PAPER PLANE 1
+  noStroke();
+  fill(255);
+  //left wing
+  triangle(plane1X, plane1Y, //right
+  plane1X-40, plane1Y+5, //top
+  plane1X-30, plane1Y+15); //bottom
+
+  //right wing
+  triangle(plane1X, plane1Y, //right
+  plane1X-25, plane1Y+20, //top
+  plane1X-10, plane1Y+35); //bottom
+
+  //grey area in middle
+    fill(220);
+  triangle(
+    plane1X, plane1Y, //right
+    plane1X-30, plane1Y+15, //top
+    plane1X-25, plane1Y+20); //bottom
+
+//grey area in middle continued
+  triangle(
+    plane1X-25, plane1Y+20, //right
+    plane1X-30, plane1Y+15, //top
+    plane1X-30, plane1Y+30 //bottom
+  );
+
+//dark area
+  fill(150);
+  triangle(
+    plane1X-20, plane1Y+25, //right
+    plane1X-25, plane1Y+20, //top
+    plane1X-30, plane1Y+30 //bottom
+  );
+  
+
+//PAPER PLANE 2
 
 
-  //plane wing background bottom part
-  quad()
+  fill(255);
+  triangle(
+    80, 90, //right
+    50, 150,
+    70, 150);
 
-  //plane body
-  quad(50, 90, //top left
-      170, 105, //top right
-      170, 110,//bottom right
-       50, 110); //bottom left
+  triangle(
+    80, 90,
+    100, 150,
+    80, 150
+  );
 
-  //plane wing foreground bottom part
-  quad(
-    95, 90, //top left
-    125, 90, //top right
-    110, 105, //bottom right
-    80, 105   //bottom left
-      )
+  noFill();
+  stroke(255);
+  line()
 
-  //plane wing foreground middle sec
-  quad(
-    140, 40, //top right
-    130, 30, //top left
-    95, 90,
-    125, 90
-  )
+
+  stroke(0, 0, 0);
+  bezier(200, 20, 10,10, 90, 90, 15, 200);
+
+
+
+
+  // //plane wing background bottom part
+  // quad()
+
+  //  //plane wing background bottom part
+  //  quad(
+  //   80, 100, //top left
+  //   110, 100, //top right
+  //   90, 125, //bottom right
+  //   70, 125 //bottom left
+  //  )
+
+  //  //plane wing background middle part
+  //  quad(70, 125, //top left
+  //   89, 125, //top right
+  //   50, 135, //bottom right
+  //   50, 135 //bottom left
+  //  )
+
+  // //plane body
+  // quad(50, 90, //top left
+  //     170, 105, //top right
+  //     170, 110,//bottom right
+  //      50, 110); //bottom left
+
+  // //plane wing foreground bottom part
+  // quad(
+  //   95, 90, //top left
+  //   125, 93, //top right
+  //   110, 105, //bottom right
+  //   80, 105   //bottom left
+  //     )
+
+  // //plane wing foreground middle sec
+  // quad(
+  //   140, 40, //top right
+  //   130, 30, //top left
+  //   95, 90,
+  //   125, 93
+  // )
+
+
+
+ 
 
 
 }
