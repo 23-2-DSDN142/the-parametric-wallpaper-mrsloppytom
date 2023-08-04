2d4d62b80e89d669ebd468482fc5dbd874839254
mrsloppytom <140354003+mrsloppytom@users.noreply.github.com>
2023-08-04 21:44:35 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,11 +2,25 @@
 let plane1X = 180; // x-coordinate of plane 1
 let plane1Y = 15; // y-coordinate of plane 1
 
-let noCloud = true;
+
+
+
+let noClouds = false ;
+
 let leftCloud = true;
+ 
+
+
+
+
+let daytime = true;
+if(daytime){
+
+}
+
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
@@ -22,6 +36,8 @@ function wallpaper_background() {
 
 
 
+
+  
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
   stroke(10, 170, 200);
@@ -29,13 +45,81 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   line(0, 180, 200, 0); //diagonal line left to right
   line(0, 0, 200, 180); //diagonal line right to left
 
-  if(noCloud){
-    background(39, 27, 64)
-    
+  fill(195, 247, 241);
+  noStroke();
+  leftCloud(20, 160, 30)
 
+
+  if(noClouds){
+    background(39, 27, 64)
+  }
+  else{
+      
   }
   
 
+  
+ let starX= 0
+ let starY=0
+ let starDi=5
+
+
+
+  function stars(starX, starY, starDi){
+
+    fill(255)
+    circle(starX, starY, starDi)
+  }
+
+  fill(255)
+  //top row
+  circle(starX, starY, starDi);
+  circle(50, starY, starDi);
+  circle(100, starY, starDi);
+  circle(150, starY, starDi);
+  circle(200, starY, starDi);
+
+  //2 row
+  circle(25, starDi*6, starDi);
+  circle(75, starDi*6, starDi);
+  circle(125, starDi*6, starDi);
+  circle(175, starDi*6, starDi);
+
+  //3 row
+  circle(0, starDi*12, starDi);
+  circle(50, starDi*12, starDi);
+  circle(100, starDi*12, starDi);
+  circle(150, starDi*12, starDi);
+  circle(200, starDi*12, starDi);
+
+  //4 row
+  circle(25, 90, starDi);
+  circle(75, 90, starDi);
+  circle(125, 90, starDi);
+  circle(175, 90, starDi);
+
+  //5 row
+  circle(0, 120, starDi);
+  circle(50, 120, starDi);
+  circle(100, 120, starDi);
+  circle(150, 120, starDi);
+  circle(200, 120, starDi);
+
+  //6 row
+  circle(25, 150, starDi);
+  circle(75, 150, starDi);
+  circle(125, 150, starDi);
+  circle(175, 150, starDi);
+
+  //7 row
+  circle(0, 180, starDi);
+  circle(50, 180, starDi);
+  circle(100, 180, starDi);
+  circle(150, 180, starDi);
+  circle(200, 180, starDi);
+  
+  
+  
 //PAPER PLANE 1
     noStroke();
     fill(255);
@@ -72,30 +156,11 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
     );
     
 
-//PAPER PLANE 2
-
-
-  // fill(255);
-  // triangle(
-  //   80, 90, //right
-  //   50, 150,
-  //   70, 150);
-
-  // triangle(
-  //   80, 90,
-  //   100, 150,
-  //   80, 150
-  // );
-
-  // noFill();
-  // stroke(255);
-  // line()
-
-
-  // stroke(0, 0, 0);
-  // bezier(200, 20, 10,10, 90, 90, 15, 200);
-
 //trail
+
+let trail = true
+
+
 
 noFill();
 stroke(255, 150, 38);
@@ -114,30 +179,6 @@ strokeWeight(3);
           100,100,
           100, 100,
           30, 180);
-// bezier(150, 45,
-//   110, 80,
-//   100, 60,
-//   100, 30)
-
-// bezier(100, 30, 
-//   100, 5,
-//   70, 100,
-//   60, 60);
-
-// bezier(60, 60,
-//   50, 10,
-//   10, 30, 
-//   30, 80);
-
-// bezier(30, 80,
-//   40, 100,
-//    50, 200,
-//   100, 130);
-
-// bezier(100, 130,
-//   160, 30,
-//   10, 80,
-//   200, 150)
 
 fill(255);
 noStroke();
@@ -154,8 +195,7 @@ noStroke();
 // let circY = 160
 // let circDi = 30
 
-fill(195, 247, 241);
-leftCloud(20, 160, 30)
+
 
 function leftCloud(circX, circY, circDi){
 
@@ -165,7 +205,6 @@ function leftCloud(circX, circY, circDi){
   circle((circDi*2)+(circX/2), circY-(circX/2), circDi)
   circle(circX+circDi, circY-(circX/2), circDi+(circX/2))
   
-  
 }
 
 
@@ -174,13 +213,11 @@ function leftCloud(circX, circY, circDi){
 
 //right cloud
 
-let circX=
-
 fill(222, 249, 252);
 circle(140, 110, 30);
 circle(150, 135, 40);
 circle(160, 115, 20);
-circle(180, 120, 10)
+circle(180, 120, 10);
 
 
 //top left cloud
@@ -193,50 +230,6 @@ circle(45, 20, 30)
 circle (70, 20, 20)
 circle(55, 20, 20)
 circle(70, 35, 30)
-
-
-
-  // //plane wing background bottom part
-  // quad()
-
-  //  //plane wing background bottom part
-  //  quad(
-  //   80, 100, //top left
-  //   110, 100, //top right
-  //   90, 125, //bottom right
-  //   70, 125 //bottom left
-  //  )
-
-  //  //plane wing background middle part
-  //  quad(70, 125, //top left
-  //   89, 125, //top right
-  //   50, 135, //bottom right
-  //   50, 135 //bottom left
-  //  )
-
-  // //plane body
-  // quad(50, 90, //top left
-  //     170, 105, //top right
-  //     170, 110,//bottom right
-  //      50, 110); //bottom left
-
-  // //plane wing foreground bottom part
-  // quad(
-  //   95, 90, //top left
-  //   125, 93, //top right
-  //   110, 105, //bottom right
-  //   80, 105   //bottom left
-  //     )
-
-  // //plane wing foreground middle sec
-  // quad(
-  //   140, 40, //top right
-  //   130, 30, //top left
-  //   95, 90,
-  //   125, 93
-  // )
-
-
 
 
 }
